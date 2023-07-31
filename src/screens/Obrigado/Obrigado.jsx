1cd406df12e6676a166939535dd27f0ebac0e066
nairut <turian@hotmail.com>
2023-07-31 17:07:54 -0300

    erte

---
+++ src/screens/Obrigado/Obrigado.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="h-1-wrapper">
+        <h1 className="text-wrapper">Obrigado!!!!!</h1>
+      </div>
+    </div>
+  );
+};
