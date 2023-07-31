e117d9f9f6d6d2b698a180770d87d764b34798ff
nairut <turian@hotmail.com>
2023-07-31 16:04:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { Celular } from "./screens/Celular";
 import { DesktopForm } from "./screens/DesktopForm";
 import { CellForm } from "./screens/CellForm";
 import { Index } from "./screens/Index";
-import { TagManager } from "../node_modules/react-gtm-module/src/TagManager";
+import { TagManager } from "react-gtm-module/src";
+
 
 
 
