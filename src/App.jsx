3d1d88925127ec2811c0e6c5d75d765f16e579c3
nairut <turian@hotmail.com>
2023-07-31 15:48:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ export const App = () => {
 
 import TagManager from 'react-gtm-module'
 const tagManagerArgs = {
-  gtmId: '<GTM-K3GKPJ5>'
+  gtmId: 'GTM-K3GKPJ5'
 }
 TagManager.initialize(tagManagerArgs)
 
